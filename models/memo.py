bc7bcb766c4373e1fcb88b19a282c4deb1d5a705
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-04-06 15:08:56 +0700

    f

--- models/memo.py
+++ models/memo.py
@@ -208,7 +208,7 @@ class MEMO(BaseLearner):
             logging.info(info)
 
     def _update_representation(self, train_loader, test_loader, optimizer, scheduler):
-        prog_bar = tqdm(range(self.args["epochs"]))####################################
+        prog_bar = tqdm(range(self.args["epochs"]))
         for _, epoch in enumerate(prog_bar):
             self.set_network()
             losses = 0.

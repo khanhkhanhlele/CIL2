746a0e0114a2bad20f7db01183f395068cc35e6f
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-07-12 19:05:05 +0700

    a

--- models/base.py
+++ models/base.py
@@ -145,11 +145,12 @@ class BaseLearner(object):
             inputs = inputs.to(self._device)
             with torch.no_grad():
                 outputs = self._network(inputs)["logits"]
-            predicts = torch.topk(
-                outputs, k=self.topk, dim=1, largest=True, sorted=True
-            )[
-                1
-            ]  # [bs, topk]
+            # predicts = torch.topk(
+            #     outputs, k=self.topk, dim=1, largest=True, sorted=True
+            # )[
+            #     1
+            # ]  # [bs, topk]
+            predicts = 0
             y_pred.append(predicts.cpu().numpy())
             y_true.append(targets.cpu().numpy())
 
